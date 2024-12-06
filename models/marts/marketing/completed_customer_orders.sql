a4412c5e80d2968422c41e62a6263b5896e6cbf7
Caitlyn O'Connor <caitlyn.anneoconnor@theinformationlab.co.uk>
2024-12-06 14:06:05 +0000

    Completed customer orders view and updated project yml

---
+++ models/marts/marketing/completed_customer_orders.sql
@@ -0,0 +1,18 @@
+with orders as (
+select * from {{ ref('stg_jaffle_shop__orders') }}
+)
+
+
+,customers as (
+    select * from {{ ref('stf_jaffle_shop_customers') }}
+)
+
+select
+    first_name
+    , last_name
+    , count(distinct order_id) as total_orders
+    from
+    customers
+    join orders on orders.customer_id = customers.customer_id
+    where orders.status = 'completed'
+    group by first_name, last_name
